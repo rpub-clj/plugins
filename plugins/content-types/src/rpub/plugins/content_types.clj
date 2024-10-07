d89927026f28a73ac4660513597a4baf0af0ebd7
Radford Smith <11401+rads@users.noreply.github.com>
2024-10-06 18:31:49 -0700

    remove code

--- plugins/content-types/src/rpub/plugins/content_types.clj
+++ plugins/content-types/src/rpub/plugins/content_types.clj
@@ -1,12 +1,11 @@
 (ns rpub.plugins.content-types
-  (:require [clojure.set :as set]
-            [rpub.admin :as admin]
+  (:require [rpub.admin :as admin]
             [rpub.model :as model]))
 
 (defprotocol Model
   (schema [model])
-  (get-fields [model opts])
-  (create-group! [model opts]))
+  (get-content-types [model opts])
+  (create-content-type! [model opts]))
 
 (defmulti ->model :db-type)
 
@@ -14,46 +13,18 @@
   [{:name "Content Types"
     :href "/admin/content-types"}])
 
-(defn fields->groups [fields]
-  (->> fields
-       (group-by :group-name)
-       (map (fn [[k v]] {:group-name k :fields v}))))
-
-(defn index-page [req]
+(defn content-types-page [req]
   (admin/page-response
     req
     {:title "Content Types"
      :primary
      (fn [{:keys [::model]}]
-       [:div
-        (admin/form
-          [:div
-           [:input {:type :text
-                    :name :group-name
-                    :placeholder "Name"}]]
-          [:div
-           [:button {:type :submit}
-            "Create Field Group"]])
-        [:div
-         (for [group (fields->groups (get-fields model {}))]
-           [:div.mt-4
-            [:h3.text-2xl.font-semibold
-             (:group-name group)]
-            [:ul
-             (for [field (:fields group)]
-               [:li (:field-name field) " (" (:field-type field) ")"])]])]])}))
+       [:div])}))
 
-(defn ->group [opts]
-  (merge {:id (random-uuid)}
-         (select-keys opts [:name])))
-
-(defn create-group [{:keys [model params] :as req}]
-  (let [opts (-> params
-                 (select-keys ["group-name"])
-                 (set/rename-keys {"group-name" :name}))
-        group (->group opts)]
-    (create-group! model group)
-    (index-page req)))
+(defn update-content-types [{:keys [model] :as req}]
+  (let [content-type {}]
+    (create-content-type! model content-type)
+    (content-types-page req)))
 
 (defn wrap-content-types [handler]
   (fn [{:keys [db-type] :as req}]
@@ -64,8 +35,8 @@
 
 (defn routes [opts]
   [["/admin/content-types" {:middleware (admin/admin-middleware opts)}
-    ["" {:get index-page
-         :post create-group}]]])
+    ["" {:get content-types-page
+         :post update-content-types}]]])
 
 (defn plugin [_]
   {:name "Content Types"
--- plugins/content-types/src/rpub/plugins/content_types/sqlite.clj
+++ plugins/content-types/src/rpub/plugins/content_types/sqlite.clj
@@ -3,51 +3,23 @@
             [rpub.model.sqlite :as sqlite]
             [rpub.plugins.content-types :as content-types]))
 
-(defn- row->field [row]
-  (update row :field-id parse-uuid))
-
-(defrecord Model
-           [ds
-            field-groups-table
-            post-types-table]
+(defrecord Model [ds content-types-table]
   content-types/Model
   (schema [_]
     [(db/strict
-       {:create-table [field-groups-table :if-not-exists]
-        :with-columns (concat [(db/uuid-column :id [:primary-key] [:not nil])
-                               [:name :text [:not nil]]]
-                              sqlite/audit-columns)})
+       {:create-table [content-types-table :if-not-exists]
+        :with-columns
+        (concat [(db/uuid-column :id [:primary-key] [:not nil])
+                 [:name :text [:not nil]]
+                 [:slug :text [:not nil]]]
+                sqlite/audit-columns)})])
 
-     (db/strict
-       {:create-table [post-types-table :if-not-exists]
-        :with-columns (concat [(db/uuid-column :id [:primary-key] [:not nil])
-                               [:name :text [:not nil]]
-                               [:slug :text [:not nil]]]
-                              sqlite/audit-columns)})])
-
-  (get-fields [_ _]
-    (->> (db/execute!
-           ds
-           {:select [[:cff.id :field-id]
-                     [:cff.content-types-group-id :group-id]
-                     [:cfg.name :group-name]
-                     [:cff.name :field-name]
-                     [:cff.type :field-type]]
-            :from [[:content-types-fields :cff]]
-            :left-join [[:content-types-groups :cfg]
-                        [:= :cfg.id :cff.content-types-group-id]]})
-         (map row->field)))
-
-  (create-group! [_ group]
-    (db/execute-one! ds {:insert-into field-groups-table
-                         :values [group]})))
+  (get-content-types [_ _])
+  (create-content-type! [_ _]))
 
 (defn ->model [opts]
-  (let [opts' (merge {:field-groups-table :content-types-groups
-                      :post-types-table :content-types-fields}
-                     (select-keys opts [:ds
-                                        :field-groups-table
-                                        :post-types-table]))]
+  (let [opts' (merge {:content-types-table :content-types}
+                     (select-keys opts [:ds :content-types-table]))]
     (map->Model opts')))
 
 (defmethod content-types/->model :sqlite [opts]
