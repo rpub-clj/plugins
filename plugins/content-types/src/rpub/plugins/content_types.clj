60615dec8f840205327bf3f86bab445cd7d269a9
Radford Smith <11401+rads@users.noreply.github.com>
2024-10-07 17:29:31 -0700

    add dynamic content types

--- plugins/content-types/src/rpub/plugins/content_types.clj
+++ plugins/content-types/src/rpub/plugins/content_types.clj
@@ -10,11 +10,37 @@
 
 (defmulti ->model :db-type)
 
-(defn menu-items [_]
-  {:content-types [{:name "Movies"
-                    :href "/admin/content-types/movies"}]
+(defn menu-items [{:keys [::model]}]
+  {:content-types (->> (get-content-types model {})
+                       (map (fn [{:keys [name slug]}]
+                              {:name name
+                               :href (str "/admin/content-types/" slug)})))
    :plugins [{:name "Content Types"
               :href "/admin/content-types"}]})
+
+(defn content-type-fields-form [{:keys [content-type]}]
+  [:div
+   (for [[i field] (map-indexed vector (sort-by :rank (:fields content-type)))]
+     [:div.mb-2.pb-2.pt-2
+      {:class (str "border-b " (when (= i 0) "border-t"))}
+      [:label {:for :field-name}]
+      [:input
+       {:type :text
+        :class (str "px-2 py-1 font-semibold border border-gray-300 "
+                    "rounded-[6px] mr-4")
+        :name :field-name
+        :value (:name field)}]
+      [:label {:for :field-type}]
+      [:select
+       {:class (str "appearance-none px-2 py-1 border border-gray-300 "
+                    "rounded-[6px] mr-4")
+        :name :field-type}
+       [:option (if (= (:type field) :text) {:selected true})
+        "Text"]
+       [:option (if (= (:type field) :number) {:selected true})
+        "Number"]]
+      (admin/button "Remove")])
+   (admin/button "Add Field")])
 
 (defn all-content-types-page [{:keys [::model] :as req}]
   (admin/page-response
@@ -22,11 +48,15 @@
     {:title "Content Types"
      :primary
      (let [content-types (->> (get-content-types model {}) (sort-by :name))]
-       (for [content-type content-types]
-         [:h3.text-2xl.font-semibold
-          (:name content-type)]))}))
+       [:div
+        [:div.mb-8 (admin/button "Add Content Type")]
+        (for [content-type content-types]
+          [:div.mb-8
+           [:h3.text-2xl.font-semibold.mb-4
+            (:name content-type)]
+           (content-type-fields-form {:content-type content-type})])])}))
 
-(defn update-content-types [{:keys [model] :as req}]
+(defn update-content-types [{:keys [::model] :as req}]
   (let [content-type {}]
     (create-content-type! model content-type)
     (all-content-types-page req)))
@@ -44,7 +74,10 @@
     (admin/page-response
       req
       {:title (:name content-type)
-       :primary [:div]})))
+       :primary
+       [:div.mb-8
+        [:h3.text-2xl.font-semibold.mb-4 "Fields"]
+        (content-type-fields-form {:content-type content-type})]})))
 
 (defn routes [opts]
   [["/admin/content-types" {:middleware (admin/admin-middleware opts)}
