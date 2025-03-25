7bef6984e3c5bb21d2838f0f8e92602f629512e9
Radford Smith <11401+rads@users.noreply.github.com>
2025-03-25 02:03:46 -0700

    Add REST API plugin

---
+++ plugins/rest-api/src/rpub/plugins/rest_api.clj
@@ -0,0 +1,155 @@
+(ns rpub.plugins.rest-api
+  (:require [camel-snake-kebab.core :as csk]
+            [camel-snake-kebab.extras :as cske]
+            [malli.core :as m]
+            [malli.error :as me]
+            [malli.transform :as mt]
+            [ring.util.response :as response]
+            [rpub.api :as api]
+            [rpub.core :as rpub])
+  (:import (java.time Instant)))
+
+(def QueryParameters
+  [:map
+   [:populate {:optional true}
+    [:or :string [:map]]]
+   [:fields {:optional true}
+    [:seqable [:string {:min 1}]]]
+   [:filters {:optional true}
+    [:map-of :string [:map-of :string :string]]]
+   [:sort {:optional true}
+    [:or :string [:seqable :string]]]
+   [:pagination
+    {:optional true}
+    [:orn
+     [:by-page
+      [:map {:closed true}
+       [:page :int]
+       [:page-size {:optional true} :int]
+       [:with-count {:optional true} :boolean]]]
+     [:by-offset
+      [:map {:closed true}
+       [:start :int]
+       [:limit {:optional true} :int]
+       [:with-count {:optional true} :boolean]]]]]])
+
+(defn- parse-query-param-arrays [params]
+  (reduce-kv
+    (fn [acc k v]
+      (cond
+        ;; Handle "key[]" format
+        (re-matches #"(.+)\[\]$" k)
+        (let [[_ base-key] (re-matches #"(.+)\[\]$" k)]
+          (assoc acc (keyword base-key) v))
+
+        ;; Handle "key[subkey]" format
+        (re-matches #"(.+)\[(.+)\]$" k)
+        (let [[_ parent-key child-key] (re-matches #"(.+)\[(.+)\]$" k)
+              parent-kw (keyword parent-key)
+              child-kw (keyword child-key)]
+          (update acc parent-kw #(assoc (or % {}) child-kw v)))
+
+        ;; Regular keys
+        :else
+        (assoc acc (keyword k) v)))
+    {}
+    params))
+
+(defn ->query-params [params]
+  (let [v (->> (parse-query-param-arrays params)
+               (cske/transform-keys csk/->kebab-case))]
+    (m/coerce #'QueryParameters v (mt/string-transformer) identity identity)))
+
+(defn- content-item->response [content-item]
+  (merge (select-keys content-item [:id :created-at :updated-at])
+         (if (:fields content-item)
+           (dissoc (:fields content-item) [:id :created-at :updated-at])
+           (:document content-item))))
+
+(defn bad-request [explanation]
+  (let [errors (->> (me/humanize explanation)
+                    (cske/transform-keys #(if (or (string? %) (keyword? %)) (csk/->camelCase %) %)))]
+    (response/bad-request {:errors errors})))
+
+(defn list-content-items-handler [{:keys [model path-params query-params] :as _req}]
+  (let [query-params' (->query-params query-params)]
+    (if-let [explanation (:explain query-params')]
+      (bad-request explanation)
+      (let [content-type-slug (keyword (:content-type-slug path-params))
+            content-items (->> (rpub/get-content-items
+                                 model
+                                 {:content-type-slugs [content-type-slug]})
+                               (map content-item->response))]
+        (response/response {:data content-items})))))
+
+(defn create-content-item-handler [{:keys [model path-params body-params current-user]}]
+  (let [content-type-slug (keyword (:content-type-slug path-params))
+        [content-type] (rpub/get-content-types
+                         model
+                         {:content-type-slugs [content-type-slug]})
+        document (-> (:data body-params)
+                     (update-keys #(parse-uuid (name %))))
+        content-item (rpub/content-item {:content-type content-type
+                                         :document document
+                                         :current-user current-user})]
+    (rpub/create-content-item! model content-item)
+    {:status 201
+     :headers {}
+     :body {:data (content-item->response content-item)}}))
+
+(defn get-content-item-handler [{:keys [model path-params] :as _req}]
+  (let [{:keys [content-item-id]} path-params
+        content-type-slug (keyword (:content-type-slug path-params))
+        content-item-id (parse-uuid content-item-id)
+        [content-item] (rpub/get-content-items
+                         model
+                         {:content-type-slugs [content-type-slug]
+                          :content-item-ids [content-item-id]})]
+    (if content-item
+      (response/response {:data (content-item->response content-item)})
+      (response/not-found nil))))
+
+(defn update-content-item-handler
+  [{:keys [model path-params body-params current-user] :as _req}]
+  (let [content-item-id (parse-uuid (:content-item-id path-params))
+        [content-item] (rpub/get-content-items
+                         model
+                         {:content-item-id [content-item-id]})
+        document (-> (:data body-params)
+                     (update-keys #(parse-uuid (name %))))
+        content-item' (-> content-item
+                          (merge {:document document
+                                  :updated-by (:id current-user)
+                                  :updated-at (Instant/now)})
+                          (select-keys [:id :document :created-at
+                                        :created-by :updated-at
+                                        :updated-by]))]
+    (rpub/update-content-item! model content-item')
+    (response/response {:data (content-item->response content-item')})))
+
+(defn delete-content-item-handler [{:keys [model path-params] :as _req}]
+  (let [content-item-id (parse-uuid (:content-item-id path-params))
+        [content-item] (rpub/get-content-items
+                         model
+                         {:content-item-ids [content-item-id]})]
+    (if content-item
+      (do
+        (rpub/delete-content-item! model content-item)
+        {:status 204, :headers {}, :body nil})
+      (response/not-found nil))))
+
+(defn routes [opts]
+  ["/api/rest" {:middleware (api/api-middleware opts)}
+   ["/{content-type-slug}"
+    {:get #'list-content-items-handler
+     :post #'create-content-item-handler}]
+   ["/{content-type-slug}/{content-item-id}"
+    {:get #'get-content-item-handler
+     :put #'update-content-item-handler
+     :delete #'delete-content-item-handler}]])
+
+(defmethod rpub/plugin ::plugin [_]
+  {:label "REST API"
+   :description "Add endpoints for a REST API. Works as a drop-in replacement for the Strapi v5 REST API."
+   :min-rpub-version "0.2.0"
+   :routes routes})
---
+++ plugins/rest-api/test/rpub/plugins/rest_api_test.clj
@@ -0,0 +1,81 @@
+(ns rpub.plugins.rest-api-test
+  (:require [babashka.fs :as fs]
+            [buddy.core.nonce :as nonce]
+            [clojure.test :refer [deftest is]]
+            [matcher-combinators.test :refer [match?]]
+            [reitit.ring :as reitit-ring]
+            [ring.mock.request :as mock]
+            [rpub.lib.db :as db]
+            [rpub.plugins.content-types.model :as ct-model]
+            [rpub.plugins.content-types.sqlite]
+            [rpub.plugins.rest-api :as rest-api]))
+
+(defn test-handler []
+  (reitit-ring/ring-handler
+    (reitit-ring/router
+      (rest-api/routes {:session-store-key (nonce/random-bytes 16)
+                        :auth-required false}))))
+
+(defn test-ds []
+  (let [temp-dir (-> (fs/create-temp-dir) fs/delete-on-exit)]
+    (db/get-datasource (format "jdbc:sqlite:%s/test.db" temp-dir))))
+
+(def test-document
+  {ct-model/slug-field-id "test-title-2"
+   ct-model/content-field-id "<p>test content 2</p>"
+   ct-model/title-field-id "test title 2"})
+
+(deftest create-test
+  (let [handler (test-handler)
+        ds (test-ds)
+        current-user {:id (random-uuid)}
+        content-types-model (ct-model/->model {:db-type :sqlite
+                                               :ds ds
+                                               :current-user current-user})
+        _ (ct-model/migrate! content-types-model)
+        body-params {:data (update-keys test-document #(keyword (str %)))}
+        create-req (-> (mock/request :post "/api/rest/posts")
+                       (merge {:model {:content-types-model content-types-model}
+                               :body-params body-params
+                               :current-user current-user}))
+        create-res (handler create-req)]
+    (is (match? {:status 201
+                 :body {:data (merge {:id uuid?, :created-at inst?}
+                                     test-document)}}
+                create-res))))
+
+(deftest delete-existing-test
+  (let [handler (test-handler)
+        ds (test-ds)
+        current-user {:id (random-uuid)}
+        content-types-model (ct-model/->model {:db-type :sqlite
+                                               :ds ds
+                                               :current-user current-user})
+        _ (ct-model/migrate! content-types-model)
+        body-params {:data (update-keys test-document #(keyword (str %)))}
+        create-req (-> (mock/request :post "/api/rest/posts")
+                       (merge {:model {:content-types-model content-types-model}
+                               :body-params body-params
+                               :current-user current-user}))
+        create-res (handler create-req)
+        delete-path (format "/api/rest/posts/%s"
+                            (get-in create-res [:body :data :id]))
+        delete-req (-> (mock/request :delete delete-path)
+                       (merge {:model {:content-types-model content-types-model}}))
+        delete-res (handler delete-req)]
+    (is (match? {:status 204} delete-res))))
+
+(deftest delete-non-existing-test
+  (let [handler (test-handler)
+        ds (test-ds)
+        current-user {:id (random-uuid)}
+        non-existing-content-item-id (random-uuid)
+        content-types-model (ct-model/->model {:db-type :sqlite
+                                               :ds ds
+                                               :current-user current-user})
+        _ (ct-model/migrate! content-types-model)
+        delete-path (format "/api/rest/posts/%s" non-existing-content-item-id)
+        delete-req (-> (mock/request :delete delete-path)
+                       (merge {:model {:content-types-model content-types-model}}))
+        delete-res (handler delete-req)]
+    (is (match? {:status 404} delete-res))))
